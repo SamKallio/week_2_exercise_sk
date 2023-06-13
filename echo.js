3bf02d831d798e0111e5dd417e2351b4d617cf10
SamKallio <sam.kalliomaki@gmail.com>
2023-06-13 18:07:17 +0300

    dd

--- echo.js
+++ echo.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/:id", (req, res) => {
-  res.json(console.log(req.params.id));
+  res.json(req.params.id);
 });
 
 module.exports = router;
